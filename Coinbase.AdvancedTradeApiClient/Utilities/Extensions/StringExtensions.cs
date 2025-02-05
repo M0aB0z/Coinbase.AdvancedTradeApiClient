1fdca71ff55d9a067758874ddb20790dfc8f8e76
M0aB0z <{ID}+{username}@users.noreply.github.com>
2025-02-05 14:30:23 +0100

    [FIX] Decimal parse with invariant culture

--- Coinbase.AdvancedTradeApiClient/Utilities/Extensions/StringExtensions.cs
+++ Coinbase.AdvancedTradeApiClient/Utilities/Extensions/StringExtensions.cs
@@ -1,8 +1,11 @@
-﻿namespace Coinbase.AdvancedTradeApiClient.Utilities.Extensions;
+﻿using System.Globalization;
+
+namespace Coinbase.AdvancedTradeApiClient.Utilities.Extensions;
 
 internal static class StringExtensions
 {
-    public static decimal ToDecimal(this string value) => decimal.Parse(value.Replace(".", ","));
+    public static decimal ToDecimal(this string value) 
+        => decimal.Parse(value, CultureInfo.InvariantCulture);
     public static decimal? ToNullableDecimal(this string value)
     {
         if (string.IsNullOrEmpty(value?.Trim()))
@@ -11,7 +14,8 @@ internal static class StringExtensions
         return value.ToDecimal();
     }
 
-    public static double ToDouble(this string value) => double.Parse(value.Replace(".", ","));
+    public static double ToDouble(this string value) 
+        => double.Parse(value, CultureInfo.InvariantCulture);
     public static double? ToNullableDouble(this string value)
     {
         if (string.IsNullOrEmpty(value?.Trim()))
